16c25c3de6b8ba6c83687f0b60a33b402eab63cf
rngtam002 <rngtam002@myuct.ac.za>
2020-08-27 18:22:48 +0200

    Add video model.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Video(models.Model):
+  """Represents a Youtube video uploaded to our database."""
+
+  name = models.CharField(max_length=100)
+  url = models.CharField(max_length=100)
+
+  def get_absolute_url(self):
+    """Returns the url to access a particular instance of Video."""
+    return reverse('video-detail-view', args=[str(self.id)])
+
+  def __str__(self):
+    """String representation of the Video object."""
+    return self.name
