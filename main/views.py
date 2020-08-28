8ca5672922fde3eb9c0f68c6ed81d0246fd82ac2
rngtam002 <rngtam002@myuct.ac.za>
2020-08-28 11:01:56 +0200

    Create View for Model

--- main/views.py
+++ main/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
+from main.models import Video, Tag
 
+def index(request):
+  num_videos = Video.objects.all().count()
+  num_tags = Tag.objects.all().count()
+
+  context = {
+    'num_videos': num_videos,
+    'num_tags': num_tags,
+  }
+
+  return render(request, 'index.html', context=context)
