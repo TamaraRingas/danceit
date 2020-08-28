930c94311abd0409033cea5d27bec58bffd06a0c
rngtam002 <rngtam002@myuct.ac.za>
2020-08-28 14:16:50 +0200

    Add accounts page path

--- danceit/urls.py
+++ danceit/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
     path('', RedirectView.as_view(url='main/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
