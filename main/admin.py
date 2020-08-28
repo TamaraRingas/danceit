dc948418d59aa55c3bc0d939992392802758f5e5
rngtam002 <rngtam002@myuct.ac.za>
2020-08-28 10:38:44 +0200

    Edit admin class

--- main/admin.py
+++ main/admin.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from .models import Video, Tag
 
 @admin.register(Video)
- class VideoAdmin(admin.ModelAdmin):
-   pass
-
-admin.site.register(Video, VideoAdmin)
+class VideoAdmin(admin.ModelAdmin):
+  pass
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
   pass
 
-admin.site.register(Tag, TagAdmin)
