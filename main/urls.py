eb2b4bf605c6deaa90c37e6981c88e92f0ef582e
rngtam002 <rngtam002@myuct.ac.za>
2020-08-28 11:41:45 +0200

    Add video-detail path

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('videos/', views.VideoListView.as_view(), name='videos'),
+    path('video/<int:pk>', views.VideoDetailView.as_view(), name='video-detail'),
 ]
