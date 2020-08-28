093db84561d6e1a8e508c5ad2fd1ffcbd137f40e
rngtam002 <rngtam002@myuct.ac.za>
2020-08-28 09:25:57 +0200

    added Tag class

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Video, Tag
 
-# Register your models here.
+admin.site.register(Video)
+admin.site.register(Tag)
+
+ 
--- main/models.py
+++ main/models.py
@@ -1,11 +1,14 @@
-from django.db import models
+ddfrom django.db import models
 from django.urls import reverse
 
 class Video(models.Model):
-  """Represents a Youtube video uploaded to our database."""
+ed  """Represents a Youtube video uploaded to our database, derived from the Model class."""
 
   name = models.CharField(max_length=100)
   url = models.CharField(max_length=100)
+  date_created = models.DateTimeField()
+
+  #tag = models.ManyToManyField("Tag")
 
   class Meta:
       ordering = ['name']
@@ -17,3 +20,23 @@ class Video(models.Model):
   def __str__(self):
     """String representation of the Video object."""
     return self.name
+
+class Tag(models.Model):
+  """Represents a Video Tag, derived from the Model class."""
+
+  name = models.CharField(max_length=20)
+  date_created = models.DateTimeField()
+
+  #video = models.ManyToManyField(Video)
+  class Meta:
+      ordering = ['name']
+
+  def get_absolute_url(self):
+    """Returns the url to access a particular Tag."""
+    return reverse('tag-detail-view', args=[str(self.id)])
+
+  def __str__(self):
+    """String representation of the Tag object."""
+    return self.name
+
+
