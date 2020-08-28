955b6f3a1814f0e763c1130f6cae0874382899c1
rngtam002 <rngtam002@myuct.ac.za>
2020-08-28 13:34:24 +0200

    Add tags to url path

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('videos/', views.VideoListView.as_view(), name='videos'),
     path('video/<int:pk>', views.VideoDetailView.as_view(), name='video-detail'),
     path('tags/', views.TagListView.as_view(), name='tags'),
+    path('tag/<int:pk>', views.TagDetailView.as_view(), name='tag-detail'),
 ]
