373188e602b498325c595df9d9dbc558195cf1e2
rngtam002 <rngtam002@myuct.ac.za>
2020-08-28 10:34:40 +0200

    Add registration decorators

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Video, Tag
 
+@admin.register(Video)
  class VideoAdmin(admin.ModelAdmin):
    pass
 
 admin.site.register(Video, VideoAdmin)
 
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
   pass
 
