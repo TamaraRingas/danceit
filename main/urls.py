9abf91186d55ccf92ec3c472806e2f7aa9884460
rngtam002 <rngtam002@myuct.ac.za>
2020-08-28 14:13:00 +0200

    Login page path

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('video/<int:pk>', views.VideoDetailView.as_view(), name='video-detail'),
     path('tags/', views.TagListView.as_view(), name='tags'),
     path('tag/<int:pk>', views.TagDetailView.as_view(), name='tag-detail'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
