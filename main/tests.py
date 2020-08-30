cb5217b3bea076bc946e7a546b32d65b8c11ac39
rngtam002 <rngtam002@myuct.ac.za>
2020-08-30 21:11:18 +0200

    Add tag model test case

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Tag 
 
-# Create your tests here.
+class TagModelTest(TestCase):
+  @classmethod
+  def setUpTestData(cls):
+    # Set up non-modified objects used by all test methods
+    Tag.objects.create(name='Balanata')
+
+  def test_tag_name(self):
+    tag = Tag.objects.get(id=1)
+    field_label = tag._meta.get_field('name').verbose_name
+    self.assertEquals(field_label, 'name')
+
