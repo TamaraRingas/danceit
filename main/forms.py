7c892a01184370de20095aee3c2c1bbd95a140c1
rngtam002 <rngtam002@cs.uct.ac.za>
2020-09-09 14:39:46 +0200

    Add Forms for adding videos

--- main/forms.py
+++ main/forms.py
@@ -3,8 +3,22 @@ from django.core.exceptions import ValidationError
 from main.models import Video, Tag, Type
 
 class AddVideoForm(forms.ModelForm):
-  class Meta:
-    model = Video
+    class Meta:
+        model = Video
+        fields = ('name', 'url', 'tags', )
+        labels = {
+            'name': 'Video Name',
+            'url': 'Video URL',
+            'tags': 'Tags',
+        }
 
-   # video_name = forms.CharField()
-    #url = forms.URLField()
+class AddTagFrom(forms.ModelForm):
+  class Meta:
+    model = Tag
+    fields = ('name', 'videos', 'types', )
+    labels = {
+      'name': 'Tag Name',
+      'videos': 'Videos',
+      'tags': 'Tags',
+    }
+
