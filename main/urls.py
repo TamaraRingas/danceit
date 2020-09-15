f07664df4f624340dfa323e03204bfab7adc0b52
rngtam002 <rngtam002@cs.uct.ac.za>
2020-09-15 11:29:05 +0200

    Map url of Type List page

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('video/<int:pk>', views.VideoDetailView.as_view(), name='video-detail'),
     path('tags/', views.TagListView.as_view(), name='tags'),
     path('tag/<int:pk>', views.TagDetailView.as_view(), name='tag-detail'),
+    path('Tag Categories/', views.TypeListView.as_view(), name = 'types'),
     path('video/create/', views.VideoCreate.as_view(), name='video_create'),
     path('video/<int:pk>/update/',
          views.VideoUpdate.as_view(), name='video_update'),
