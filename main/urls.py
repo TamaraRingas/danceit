4fe3beaf8a9b1a91cb00d2b997c0c8aefe9d8a89
rngtam002 <rngtam002@cs.uct.ac.za>
2020-09-28 17:03:43 +0200

    Remove old url path from branch

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('youtube/', views.youtube_view, name='youtube'),
     path('videos/', views.VideoListView.as_view(), name='videos'),
     path('video/<int:pk>', views.VideoDetailView.as_view(), name='video-detail'),
     path('youtube', views.search_youtube, name='youtube_search'),
