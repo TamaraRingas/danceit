635f447b27576e48b0b024533ef0805d1311f045
rngtam002 <rngtam002@cs.uct.ac.za>
2020-09-28 17:00:00 +0200

    Make API search from html form and test

--- main/views.py
+++ main/views.py
@@ -50,38 +50,40 @@ def video_search(request, query):
   return render(request, 'main/video_list.html', context=context)
 
 def search_youtube(request):
-  search_url = 'https://www.googleapis.com/youtube/v3/search'
-  video_url = 'https://www.googleapis.com/youtube/v3/videos'
-  search_params = {
-    'part': 'snippet',
-    'q': 'dnd bard guide',
-    'key': settings.YOUTUBE_DATA_API_KEY,
-    'type': 'video',
-  }
-  video_ids = []
-  r = requests.get(search_url, params=search_params)
-  results = r.json()['items']
-  for result in results:
-    video_ids.append(result['id']['videoId'])
-  
-  video_params = {
-    'key': settings.YOUTUBE_DATA_API_KEY,
-    'part': 'snippet, contentDetails',
-    'id': ','.join(video_ids)
-  }
-  r = requests.get(video_url, params=video_params)
-
-  results = r.json()['items']
   videos = []
-  for result in results:
-    video_data = {
-        'title': result['snippet']['title'],
-        'id': result['id'],
-        'url': f'https://www.youtube.com/watch?v={result["id"]}',
-        'duration': parse_duration(result['contentDetails']['duration']),
-        'thumbnail': result['snippet']['thumbnails']['high']['url']
+  if request.method == 'POST': 
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    video_url = 'https://www.googleapis.com/youtube/v3/videos'
+    search_params = {
+      'part': 'snippet',
+      'q': request.POST['search'],
+      'key': settings.YOUTUBE_DATA_API_KEY,
+      'type': 'video',
     }
-    videos.append(video_data)
+    video_ids = []
+    r = requests.get(search_url, params=search_params)
+    results = r.json()['items']
+    for result in results:
+      video_ids.append(result['id']['videoId'])
+    
+    video_params = {
+      'key': settings.YOUTUBE_DATA_API_KEY,
+      'part': 'snippet, contentDetails',
+      'id': ','.join(video_ids)
+    }
+    r = requests.get(video_url, params=video_params)
+
+    results = r.json()['items']
+    
+    for result in results:
+      video_data = {
+          'title': result['snippet']['title'],
+          'id': result['id'],
+          'url': f'https://www.youtube.com/watch?v={result["id"]}',
+          'duration': parse_duration(result['contentDetails']['duration']),
+          'thumbnail': result['snippet']['thumbnails']['high']['url']
+      }
+      videos.append(video_data)
   context = {
     'videos': videos
   }
