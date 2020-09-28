0a4a9dfab2f68be9681c6297c2d5ccdf96d07f65
rngtam002 <rngtam002@cs.uct.ac.za>
2020-09-28 15:26:58 +0200

    Map YouTube search url

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('videos/', views.VideoListView.as_view(), name='videos'),
     path('video/<int:pk>', views.VideoDetailView.as_view(), name='video-detail'),
+    path('youtube', views.search_youtube, name='youtube_search'),
     path('tags/', views.TagListView.as_view(), name='tags'),
     path('tag/<int:pk>', views.TagDetailView.as_view(), name='tag-detail'),
     path('Tag Categories/', views.TypeListView.as_view(), name = 'types'),
