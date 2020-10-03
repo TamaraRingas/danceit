ed6ec6b3ca256393b4fec92faac3c8b5f9c9cf05
rngtam002 <rngtam002@cs.uct.ac.za>
2020-10-03 22:25:26 +0200

    Edit AddVideoForm

--- main/forms.py
+++ main/forms.py
@@ -9,16 +9,16 @@ from main.models import Video, Tag, Type
 
 class AddVideoForm(ModelForm):
 
-    def clean_name(self):
+    def clean_name(self): #Override clean_data method from ModelForm class.
       data = self.cleaned_data['name']
 
-      if len(data) > 100: 
-          raise ValidationError(_('Invalid Video Name'))
+      if len(data) > 100: #Check that name entered is valid.
+          raise ValidationError(_('Invalid Video Name')) #If not, raise error.
       
       return data
 
-    def clean_url(self):
-        url_data = self.cleaned_data['url']
+    def clean_url(self): #COverride clean_data method for the data in each field.
+        url_data = self.cleaned_data['url'] 
         #
         return url_data
         
@@ -28,15 +28,14 @@ class AddVideoForm(ModelForm):
         return tag_data
 
     class Meta:
-        model = Video
-        fields = ['name', 'url', 'tags', ]
-        labels = {
+        model = Video #Set model class.
+        fields = ['name', 'url', 'tags', ] #List which fields are used in form.
+        labels = { #Set field labels
             'name': 'Video Name',
             'url': 'Video URL',
             'tags': 'Tags',
         }
 
-    
 class AddTagForm(forms.ModelForm):
     def clean_name(self):
         data = self.cleaned_data['name']
@@ -53,7 +52,7 @@ class AddTagForm(forms.ModelForm):
 
     def clean_tags(self):
         type_data = self.cleaned_data['types']
-        #
+        
         return tag_data
     class Meta:
         model = Tag
