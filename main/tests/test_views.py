d80c06f5e0e22033f39c1c7d9acccbdf5e8d5f44
rngtam002 <rngtam002@cs.uct.ac.za>
2020-10-03 13:41:51 +0200

    Add template test method

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -20,3 +20,8 @@ class VideoListViewTest(TestCase):
   def test_view_url_accessible_by_name(self):
     response = self.client.get(reverse('videos'))
     self.assertEqual(response.status_code, 200)
+
+  def test_view_uses_correct_template(self):
+      response = self.client.get(reverse('videos'))
+      self.assertEqual(response.status_code, 200)
+      self.assertTemplateUsed(response, 'main/video_list.html')
